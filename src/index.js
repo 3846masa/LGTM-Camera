8a191ca0ea15d6b3ed25bbccf30d6e703acb0f59
3846masa <3846masahiro+git@gmail.com>
2016-09-25 23:55:48 +0900

    Fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 
-if (process.env.NODE_ENV !== 'production') {
+if (
+  process.env.NODE_ENV !== 'production' &&
+  window.__VUE_DEVTOOLS_GLOBAL_HOOK__
+) {
   window.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = Vue;
   Vue.config.devtools = true;
 }
