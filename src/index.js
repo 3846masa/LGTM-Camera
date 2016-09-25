b8d2019b918890d3be45ba0eabbdfb95572706ad
3846masa <3846masahiro+git@gmail.com>
2016-09-25 23:09:45 +0900

    Setup for develop

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@
 import Vue from 'vue';
 import App from './App.vue';
 
-Vue.config.devtools = true;
+if (process.env.NODE_ENV !== 'production') {
+  window.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = Vue;
+  Vue.config.devtools = true;
+}
+
 new Vue(App).$mount('App');
